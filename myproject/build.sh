6b6538b9b8d4049177d6055abce28e521899c1b7
KAVI7871281698 <kavipriyan292004@gmail.com>
2025-12-29 18:03:30 +0530

    create the superuser

--- myproject/build.sh
+++ myproject/build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
 python manage.py migrate --noinput
+
+if [ "$CREATE_SUPERUSER" = "True" ]; then
+  python manage.py createsuperuser \
+    --noinput \
+    --username "$DJANGO_SUPERUSER_USERNAME" \
+    --email "$DJANGO_SUPERUSER_EMAIL"
+fi
+
 python manage.py collectstatic --noinput
