7f674b3f26b0e4682dd2f76d0fcbbc48eb95cc5a
KAVI7871281698 <kavipriyan292004@gmail.com>
2025-12-31 11:12:21 +0530

    Delete that feature is wrong implemented

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -131,7 +131,6 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-    'RESOURCE_TYPE': 'auto', # This will allow all file types
 }
 
 # --------------------------------------------------
